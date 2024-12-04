3d9cf1f5644c2042c7d21b3a481e117460a7464a
Hoja dev <devhoja@gmail.com>
2024-12-04 11:44:54 -0300

    my en textos

--- src/components/index/VinetasIndex.tsx
+++ src/components/index/VinetasIndex.tsx
@@ -12,22 +12,22 @@ export default function VinetasIndex(){
             />
 
             <div className="flex flex-col justify-center items-center">
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                     La ansiedad podría manifestarse como un malestar persistente, tanto físico como
                     psicológico, que sería una respuesta a una amenaza futura.
                 </p>
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                     Se puede reconocer en momentos de nerviosismo y preocupación, con síntomas bastantes comunes como la
                     inquietud, fatiga, tensión muscular, sudoración, dificultad para controlar pensamientos,
                     sentir un nudo en la garganta, aceleración del corazón, etc…<br/><br/>
                 </p>
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                     Pero esencialmente se define como un malestar significativo que podría afectar tu vida diaria.
                     Acá te enseñaremos algunas técnicas personales por si tienes una crisis de pánico o ansiedad, pero primero…
                </p>
             </div>
 
-            <h2 className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-regular text-3xl lg:text-5xl text-center mb-12 text-[#321C14]`}>
+            <h2 className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-regular text-3xl lg:text-5xl text-center my-4 text-[#321C14]`}>
                 ¿SABES COMO AYUDAR A ALGUIEN CON UNA CRISIS DE ANSIEDAD / PÁNICO?
             </h2>
 
@@ -48,20 +48,20 @@ export default function VinetasIndex(){
             {/* Ejercicios de respiracion */}
 
             <div className="flex flex-col justify-center items-center">
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                     Los ejercicios de respiración son una de las herramientas más efectivas en
                     estos casos, puede que tocar a la persona en cuestión empeore la situación,
                     por lo que se recomienda cuidar el espacio personal… Quizá como máximo
                     tomate de las manos para que concentre su atención en ellas y guiarle para recuperar el control.
                 </p>
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                     A pesar de haber una guía amplia de respiraciones, a veces la persona se le
                     suele olvidar estando en una crisis, pero centrar al individuo para ayudarle a
                     hacer una serie de 5 respiraciones profundas ayuda bastante.<strong> ¡Puede costar
                     al principio, por lo que el cariño y la paciencia en estos casos son clave!</strong>
                 </p>
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-4xl lg:text-6xl text-center mb-12 text-[#321C14]`} > * </p>
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-lg lg:text-2xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-4xl lg:text-6xl text-center my-4 text-[#321C14]`} > * </p>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-lg lg:text-2xl text-center my-4 text-[#321C14]`}>
                     <strong>Ahora unos ejercicios de respiración y tips que pueden servir en algún apuro…</strong>
                </p>
             </div>
@@ -71,11 +71,11 @@ export default function VinetasIndex(){
 				alt="Respiracion "/>
 
             <div className="flex flex-col justify-center items-center">
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                     La respiración diafragmática consiste en respirar profundo usando el
                     diafragma, o sea, con el estómago y no con el pecho.
                 </p>
-                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+                <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                     Inhalas lento dejando que el abdomen se expanda y luego exhalar despacio.
                     Esto ayuda a calmar el cuerpo porque activa el sistema nervioso
                     parasimpático, que induce una sensación de calma bajando los niveles de
@@ -90,7 +90,7 @@ export default function VinetasIndex(){
 				src={"https://res.cloudinary.com/dmlrehiwh/image/upload/v1732501708/kqbs4medqhz1y1hv6tdm.png"}
 				alt="Logo del proyecto Desprender"/>
 
-            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                 Esta respiración es bastante simple: Inhalas por la nariz contando hasta 4, luego aguantas
                 la respiración contacto hasta 7 y exhalas lentamente por la boca durante 8 segundos. Se
                 recomienda colocar la boca simulando un pequeño silbido. Esta técnica ayuda a reducir el
@@ -102,19 +102,19 @@ export default function VinetasIndex(){
 				src={"https://res.cloudinary.com/dmlrehiwh/image/upload/v1732501706/fsyulpeuprnixpmtpmny.png"}
 				alt="Logo del proyecto Desprender"/>
 
-            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                 Si estás en medio de una posible crisis, este tipo te ayudará a poder concentrarte en dos objetos.
                 Está bien si cuesta al principio, pero concéntrate bien, compara dos objetos que estén a tu
                 alcance y verás cómo tu cuerpo y mente vuelven. Hacer hasta sentirse bien
             </p>
-            <h2 className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-regular text-3xl lg:text-5xl text-center mb-12 text-[#321C14]`}>
+            <h2 className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-regular text-3xl lg:text-5xl text-center my-4 text-[#321C14]`}>
                 Tip del limón
             </h2>
             <Vineta
 				src={"https://res.cloudinary.com/dmlrehiwh/image/upload/v1732501706/rlxmywwi0ergfenofyma.png"}
 				alt="Logo del proyecto Desprender"/>
 
-            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center mb-12 text-[#321C14]`}>
+            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
             Andar con un limón en la cartera suena chistoso, pero en medio de una crisis el limón es una
             herramienta útil, ya que con solo un mordisco la sensación ácida hará que pongas atención a eso y 
             termines poniendo los pies en la tierra.
@@ -124,7 +124,7 @@ export default function VinetasIndex(){
 				src={"https://res.cloudinary.com/dmlrehiwh/image/upload/v1733279456/pg2o1ihe6f3gr5ipiiaq.png"}
 				alt="Logo del proyecto Desprender"/>
 
-            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-12  text-[#321C14]`}>
+            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                 Si no tienes limón, un dulce ácido igual estaría bien.
             </p>
 
@@ -132,17 +132,17 @@ export default function VinetasIndex(){
 				src={"https://res.cloudinary.com/dmlrehiwh/image/upload/v1733279456/o0vhynsd8xhydlqocbna.png"}
 				alt="Logo del proyecto Desprender"/>
 
-            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-12 text-[#321C14]`}>
+            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
                 Pon tus manos bajo agua helada, esto hará que tu cerebro enfoque su atención en otros estímulos.
             </p>
 
             {/* Te gusta el arte? */}
 
-            <h2 className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-regular text-2xl lg:text-4xl text-center mb-12 text-[#321C14]`}>
+            <h2 className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-regular text-2xl lg:text-4xl text-center my-4 text-[#321C14]`}>
                 ¿TE GUSTA EL ARTE?
             </h2>
 
-            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-12 text-[#321C14]`}>
+            <p className={`${gloria.className} w-[80%] md:w-[65%] lg:w-[50%] font-light text-base lg:text-xl text-center my-4 text-[#321C14]`}>
             El arte es significativo para muchos, funciona como expresión emocional y las obras de otras
             personas también podrían conmoverte. Hacerle sentido a alguien que conecte con lo que observas.
             </p>
