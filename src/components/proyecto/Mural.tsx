7686d4d0c951e43897987497fcfe99c0d7be0f11
Hoja dev <devhoja@gmail.com>
2024-12-15 03:04:53 -0300

    text black, proyecto

--- src/components/proyecto/Mural.tsx
+++ src/components/proyecto/Mural.tsx
@@ -14,7 +14,7 @@ export default function Mural({src, alt, subtitle}: MuralProps){
                 className="w-[100dvw] md:w-[70dvw] xl:w-[50dvw] self-center"
                 loading="lazy">	
             </Image>
-            <h4 className={`${modak.className} text-xl xl:text-5xl text-[#DDA11A] text-graff ml-2 md:w-[80%] self-center mt-4`}>{subtitle}</h4>
+            <h4 className={`${modak.className} text-xl xl:text-5xl text-black ml-2 md:w-[80%] self-center mt-4`}>{subtitle}</h4>
         </div>
     ) 
 }
